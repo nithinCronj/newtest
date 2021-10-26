453cf93424f061ebd42773110258da117e55cefe
nithinCronj <nithin@cronj.com>
2021-10-26 22:19:31 +0530

    Employee using express

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const produncts =require('./products.json')
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", function(error) {
+    console.error(error);
+    });
+
+const app = express()
+app.use(express.json())
+const port = 3000
+
+app.get('/', (req, res) => {
+  //res.send('Hello World!')
+ res.send(JSON.stringify(produncts));
+})
+app.post("/addEmp", (req,res)=>
+{
+
+    console.log(req.body);
+    const id = req.body.id;
+    const name = req.body.name;
+    const age = req.body.age;
+    const employee={
+        id: id,
+        name: name,
+        age:age
+    }
+    client.set(id, JSON.stringify(employee), redis.print);
+    res.send({success:true})
+})
+
+app.get("/getEmp/:id", (req,res)=>{
+    const id= req.param("id");
+    console.log(id);
+    client.get(id,(err,data)=>{
+   
+console.log(data);
+    //console.log(req.param("id"));
+    res.send(JSON.parse(data))
+    
+})
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+const admin = {name:"nithi",pass:"123"}
+
+app.post("/login", (req,res)=>{
+    const name= req.body.name;
+    const password = req.body.password;
+console.log(name,password);
+if(name == admin.name && password==admin.pass){
+res.send({success: true})
+}
+else{
+    res.send({success:false})
+}
+})
+
+
+
