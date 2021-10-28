965d834695e4e555dbd34bcb5953a6abad7a103c
nithinCronj <nithin@cronj.com>
2021-10-28 21:00:26 +0530

    Almost working fine

--- newNode.js
+++ newNode.js
@@ -8,58 +8,56 @@ client.on("error", function(error) {
   console.error(error);
 });
 
-
-
-employeeDetails = {"1":{"name":"Nithin", "age":25, "email":"nithin@cronj.com"},
+employeeDetails = 
+{"1":{"name":"Nithin", "age":25, "email":"nithin@cronj.com"},
 "2":{"name":"aditya", "age":25, "email" :"aditya@cronj.com"}}
 
-admincred = {
+admin = 
+{
     user: "nithi",
     pass: "123"
 }
 
-function getRouteFromUrl(url){
+function getroute(url)
+{
    const r= url.split("/")[1].split("?")[0];
    console.log(r);
-   return r
-   
-   
+   return r   
 }
 
-function getQuery(request){
+function getQuery(request)
+{
     var url_parts = url.parse(request.url, true);
     request.query = url_parts.query;
 }
 
 callBackFunction = (req, res)=>{
-    res.writeHead(200, {'Content-Type': 'application/json'});  //middleware
+    res.writeHead(200, {'Content-Type': 'application/json'}); 
     getQuery(req);
     switch(req.method){
         case "POST":
-                switch(getRouteFromUrl(req.url)){
+                switch(getroute(req.url)){
                     case "add":
-                    {
+                        {
                         var reqEmp="";
                         req.on('data',(data)=>{
                             reqEmp+=data;
-                        
-                        })
+                                            })
                         req.on('end', () =>
-                        {
+                            {
                             let reqData= JSON.parse(reqEmp.toString());
-                            console.log(reqData);
+                            //console.log(reqData);
                             client.set(JSON.stringify(reqData.email), JSON.stringify(reqData) ,(err, data)=>{
-                                if(err){
+                            if(err){
                                     console.log(data)
-                                    res.end("error")
-                                 
-                                }else{
+                                    res.end("error")     
+                                    }
+                                else{
                                     res.end(JSON.stringify(reqData))
-                                }
-                               
-                            })
+                                    }
+                                                                                                })
                             
-                        })
+                                    })
                         break;
                     }
                     case "login":{
@@ -68,36 +66,32 @@ callBackFunction = (req, res)=>{
                         var requestData = ""
                         req.on('data', (data)=>{
                             requestData+=data;
-                        })
+                                                })
                         req.on('end', ()=>{
-                            let parsedReq = JSON.parse(requestData.toString()); //deserializeer
-                            console.log("parse req",parsedReq)
-                            if( parsedReq.user == admincred.user && parsedReq.pass == admincred.pass){
-                               
+                            let parsedReq = JSON.parse(requestData.toString()); 
+                            //console.log("parse req",parsedReq)
+                            if( parsedReq.user == admin.user && parsedReq.pass == admin.pass){  
                                // res.end(JSON.stringify({success:true})) //serializers
-
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({success:true}));
-    res.end();
-                            }else{
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.write(JSON.stringify({success:true}));
+                                    res.end();
+                            }
+                            else{
                                 res.end(JSON.stringify({success:false}))
                             }
                         })
                         break;
                     }
                     default : {
-                        res.write("default home")
-                        res.end()
+                        res.end("Error")
                         break
                     }
                 }
             break;
 
         case "GET":
-            switch(getRouteFromUrl(req.url)){
+            switch(getroute(req.url)){
                 case "employee":{
-              
-
                     const {id} = req.query;
                     console.log(id);
                     const r=employeeDetails[id]
